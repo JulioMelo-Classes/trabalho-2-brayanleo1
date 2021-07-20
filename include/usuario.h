6d2fb08211d1fe7dd3f467f0fca9f9bdc53ac267
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-07-20 17:15:21 -0300

    classe usuário implementada

--- include/usuario.h
+++ include/usuario.h
@@ -8,9 +8,50 @@ class Usuario{
 		string nome; //<! O nome cadastrado pelo usuário
 		string email; //<! O email cadastrado pelo usuário
 		string senha; //<! A senha cadastrada pelo usuário
-	/*
-		TODO implementação de métodos
-	*/
+	public:
+
+		//*! Construtor de usuário que cria um usuário vazio
+		Usuario();
+
+		/*! Coloca o id do usuário
+				@param id_ Id do usuário a ser criado
+		*/
+		void setId(int id_);
+
+		/*! Recupera o id do usuário
+				@return Um int que representa o id do usuário
+		*/
+		int getId();
+
+		/*! Coloca o nome do usuário
+				@param nome_ nome do usuário a ser criado
+		*/
+		void setNome(string nome_);
+
+		/*! Recupera o nome do usuário
+				@return Uma string que representa o nome do usuário
+		*/
+		string getNome();
+
+		/*! Coloca o email do usuário
+				@param email_ endereço de email do usuário a ser criado
+		*/
+		void setEmail(string email_);
+
+		/*! Recupera o email do usuário
+				@return Uma string que representa o email do usuário
+		*/
+		string getEmail();
+
+		/*! Coloca a senha do usuário
+				@param senha_ senha do usuário a ser criado
+		*/
+		void setSenha(string senha_);
+
+		/*! Recupera a senha do usuário
+				@return Uma string que representa a senha do usuário
+		*/
+		string getSenha();
 };
 
 #endif
---
+++ src/usuario.cpp
@@ -0,0 +1,37 @@
+#include "usuario.h"
+
+using namespace std;
+
+/* COMANDOS */
+Usuario::Usuario(){}
+
+void Usuario::setId(int id_){
+    id = id_;
+}
+
+int Usuario::getId(){
+    return id;
+}
+
+void Usuario::setNome(string nome_){
+    nome = nome_;
+}
+
+string Usuario::getNome(){
+    return nome;
+}
+void Usuario::setEmail(string email_){
+    email = email_;
+}
+
+string Usuario::getEmail(){
+    return email;
+}
+
+void Usuario::setSenha(string senha_){
+    senha = senha_;
+}
+
+string Usuario::getSenha(){
+    return senha;
+}
